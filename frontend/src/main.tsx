1010c21ce6c9b5f1985d6ec6bda31a0b4e690dc7
Yash81300 <yashmalik81300@gmail.com>
2026-04-09 11:19:04 +0530

    Add health check fetch request on startup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+fetch(`${import.meta.env.VITE_API_URL}/health`).catch(() => {})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
